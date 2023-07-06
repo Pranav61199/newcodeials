685388756c34f6affe7bb29444f70998f8823165
Pranav D <pranavdeshmukh61199@gmail.com>
2023-07-06 15:36:11 +0530

    code upto cookies

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const app=express();
 const port=1000;
 const expresslayouts=require('express-ejs-layouts');
 const db=require('./config/mongoose');
+const cookieparser=require('cookie-parser');
 
+
+app.use(express.urlencoded());
+app.use(cookieparser());
 app.use(express.static('./assets'));
 
 app.use(expresslayouts);
